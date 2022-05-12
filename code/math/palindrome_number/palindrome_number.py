733286f50951ae92beff8f070f0651ae944453b8
Tianxiao Li <53313027+tonyli1121@users.noreply.github.com>
2022-05-11 21:58:30 -0400

    Update palindrome_number.py

--- code/math/palindrome_number/palindrome_number.py
+++ code/math/palindrome_number/palindrome_number.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # If x is a negative number it is not a palindrome
+        # If x % 10 = 0, in order for it to be a palindrome the first digit should also be 0
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+                
+        reversed_num = 0
+        while reversed_num < x:
+            reversed_num = reversed_num * 10 + x % 10
+            x = x // 10
+        return (reversed_num == x or reversed_num // 10 == x)
