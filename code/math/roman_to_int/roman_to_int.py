7feabc6575ec4dede8da96645f195715c0a40428
Tianxiao Li <53313027+tonyli1121@users.noreply.github.com>
2022-05-14 14:36:59 -0400

    Update roman_to_int.py

--- code/math/roman_to_int/roman_to_int.py
+++ code/math/roman_to_int/roman_to_int.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        lookup_dict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        res = 0
+        for i in range(len(s)):
+            # for case IV, IX, XL, XC, CD, CM we need to subtract the previous char
+            if i + 1 < len(s) and lookup_dict[s[i]] < lookup_dict[s[i+1]]:
+                res -= lookup_dict[s[i]]
+            else:
+                res += lookup_dict[s[i]]
+        return res
